289a083314825b2d6e9baba0d299879d37be5eeb
JJKOrlino <jjkorlino80@yahoo.com>
2020-11-18 16:59:56 -0600

    commit tree c698896379ce29e701391d2df919c52db7857940

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+end
+
+fido = Dog.new
+
+class Dog
+    def bark
+        puts "Woof!"
+    end
+end
+fido.bark
+
+class Dog
+    def sit
+        puts "The Dog is sitting"
+    end
+end
+fido.sit
---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+end
+
+ada = Person.new
+
+class Person
+    def talk
+        puts "Hello World!"
+    end
+end
+ada.talk
+
+class Person
+    def walk
+        puts "The Person is walking"
+    end
+end
+ada.walk
